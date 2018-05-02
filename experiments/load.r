f50d9493a13106d086b7ac312c43992fa0ac7fb8
John Paul Guzman <john_paul_guzman@dlsu.edu.ph>
2018-05-01 19:25:27 -0700

    do arcs analysis

--- experiments/load.r
+++ experiments/load.r
@@ -2,33 +2,52 @@ rm(list=ls())
 library("R.utils")
 library("bnlearn")
 
-## PARAMETERS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-## >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+## SET UP >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+## >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 setwd("C:/Users/JP/Desktop/LAGT_data/experiments/")
 saved_networks = list()
+saved_parameters = list()
 read_timeout = 15
 expected_file = "BN15a-B1.dsc"
+empirical_dataset_1 = "data_exam1.csv"
+empirical_dataset_2 = "data_exam2.csv"
+
+## Process files
+expected_network = bn.net(read.dsc(expected_file))
+empirical_data_1 = read.csv(empirical_dataset_1, check.names=FALSE)
+empirical_data_2 = read.csv(empirical_dataset_2, check.names=FALSE)
+
+# Filter noise 
 folder_path = "./experiments_2/"
-## <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-## PARAMETERS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+empirical_data = rbind(empirical_data_1[0, ], empirical_data_2)
+empirical_data = within(empirical_data, rm("ID"))
+
+## <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+## SET UP <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 expected_network = bn.net(read.dsc(expected_file))
 detected_files = list.files(path=folder_path, pattern="*.dsc")
 
 for (file in detected_files) {
     filepath = paste(folder_path, file, sep="")
+    idx = substr(file, 1, nchar(file) - 4)
     tryCatch({
-        saved_networks[[substr(file, 1, nchar(file) - 4)]] = withTimeout(
-            bn.net(read.dsc(filepath, debug=FALSE)),
-            timeout=read_timeout)
+        saved_parameters[[idx]] = withTimeout(read.dsc(filepath, debug=FALSE), timeout=read_timeout)
+        saved_networks[[idx]] = withTimeout(bn.net(saved_parameters[[idx]]), timeout=read_timeout)
         print(paste("Loaded:", filepath))
     }, error=function(e){print(paste("!! ERROR :", conditionMessage(e), "\n"))})
 }
 
+print("===== DATA FOR STRUCTURAL DIFFERENCES & CROSS VALIDATION =====")
 for (idx in names(saved_networks)) {
     net_hamming = hamming(expected_network, saved_networks[[idx]])
     net_shd = shd(expected_network, saved_networks[[idx]])
-    print(paste(">> Index:", idx, " -- || -- Hamming distance:", net_hamming, ", Structural Hamming distance:", net_shd))
+    cv_runs = bn.cv(data=empirical_data, bn=saved_networks[[idx]])
+    mean_error = mean(sapply(cv_runs, function(x) attr(x, "mean")))
+    print(paste(">> Index:", idx, " -- || -- Hamming distance:", net_hamming, ", Structural Hamming distance:", net_shd, " -- || -- Mean error:", mean_error))
 }
 
-# bn.cv(data=empirical_data, bn="pc.stable", k=10)
+cv_runs2 = bn.cv(data=empirical_data, bn=expected_network)
+mean_error2 = mean(sapply(cv_runs2, function(x) attr(x, "mean")))
+print(paste(">> Index: Expected Network -- || -- Mean error:", mean_error2))
--- experiments/main.r
+++ experiments/main.r
@@ -5,8 +5,9 @@ rm(list=ls())
 library("R.utils")
 library("bnlearn")
 
-## PARAMETERS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-## >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+## SET UP >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+## >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 setwd("C:/Users/JP/Desktop/LAGT_data/experiments/")
 max_time = 120
 learned_networks = list()
@@ -20,12 +21,11 @@ empirical_data_1 = read.csv(empirical_dataset_1, check.names=FALSE)
 empirical_data_2 = read.csv(empirical_dataset_2, check.names=FALSE)
 
 # Filter noise 
-empirical_data_1 = empirical_data_1[0, ]
-## <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-## PARAMETERS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-empirical_data = rbind(empirical_data_1, empirical_data_2)
+empirical_data = rbind(empirical_data_1[0, ], empirical_data_2)
 empirical_data = within(empirical_data, rm("ID"))
+
+## <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+## SET UP <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 ## Check if nodes match
 expected_columns = sort(nodes(expected_network))
@@ -38,7 +38,6 @@ if (!Reduce("&", match_columns)) { # checks if all columns match
 }
 
 ## Test multiple structural algorithms
-if (TRUE){
 constraint_based_algorithms = c("pc.stable", "gs", "iamb", "fast.iamb", "inter.iamb", "mmpc", "si.hiton.pc")
 independence_tests = c("mi", "mi-adf", "mc-mi", "smc-mi", "sp-mi", "mi-sh", "x2", "x2-adf", "mc-x2", "smc-x2", "sp-x2")
 for (algorithm in constraint_based_algorithms){ for (test in independence_tests) {
@@ -61,9 +60,7 @@ for (algorithm in constraint_based_algorithms){ for (test in independence_tests)
         print(paste(empirical_file, "already exists!"))
     }
 }}
-}
 
-if (TRUE){
 score_based_algorithms = c("hc", "tabu")
 scores = c("loglik", "aic", "bic", "bde", "bds", "bdj", "k2", "mbde", "bdla")
 for (algorithm in score_based_algorithms){ for (score in scores) {
@@ -86,4 +83,3 @@ for (algorithm in score_based_algorithms){ for (score in scores) {
         print(paste(empirical_file, "already exists!"))
     }
 }}
-}
